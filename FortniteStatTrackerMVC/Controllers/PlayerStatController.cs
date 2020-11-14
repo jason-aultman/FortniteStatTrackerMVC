3141c0e81b09ac072110033b117123c122cab6c3
jason-aultman <jason.aultman@gmail.com>
2020-11-14 09:01:07 -0500

    Add project files.

---
+++ FortniteStatTrackerMVC/Controllers/PlayerStatController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FortniteStatTrackerMVC.Controllers
+{
+    public class PlayerStatController : Controller
+    {
+        public readonly HttpClient _httpClient;
+        public readonly JsonSerializerOptions _options;
+        public PlayerStatController()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://api.fortnitetracker.com/v1/");
+            _options = new JsonSerializerOptions() { WriteIndented = true, PropertyNameCaseInsensitive = true };
+        }
+        // GET: PlayerStatController
+        public async Task<ActionResult> Index(string Id)
+        {
+            var player = new PlayerStat();
+            List<LifeTimeStat> stats = player.LifeTimeStats;
+            _httpClient.DefaultRequestHeaders.Add("TRN-Api-Key", "62cd4ae1-486d-4f51-8046-15084a4fe764");
+
+            var response = await _httpClient.GetAsync($"profile/gamepad/{Id}");
+            if(response.IsSuccessStatusCode)
+            {
+                var parseRawToJson = await response.Content.ReadAsStringAsync();
+                var parsedToPlayerStat = JsonSerializer.Deserialize<PlayerStat>(parseRawToJson, _options);
+                player = parsedToPlayerStat;
+                if(player.EpicUserHandle!=null)
+                {
+                    return View(player);
+                }
+            }
+            
+            return NotFound();
+
+            
+        }
+        public IActionResult ErrorPlayerNotFound()
+        {
+            return View();
+        }
+
+        // GET: PlayerStatController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PlayerStatController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PlayerStatController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PlayerStatController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PlayerStatController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PlayerStatController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PlayerStatController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ FortniteStatTrackerMVC/Models/PlayerStat.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace FortniteStatTrackerMVC
+{
+
+    public partial class PlayerStat
+    {
+        public Guid AccountId { get; set; }
+        public Uri Avatar { get; set; }
+        public long PlatformId { get; set; }
+        public string PlatformName { get; set; }
+        public string PlatformNameLong { get; set; }
+        public string EpicUserHandle { get; set; }
+        [NotMapped]
+        public Dictionary<string, Dictionary<string, Stat>> Stats { get; set; }
+        public List<LifeTimeStat> LifeTimeStats { get; set; }
+        public List<RecentMatch> RecentMatches { get; set; }
+    }
+
+    public partial class LifeTimeStat
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+
+    public partial class RecentMatch
+    {
+        public long Id { get; set; }
+        public Guid AccountId { get; set; }
+        public string Playlist { get; set; }
+        public long Kills { get; set; }
+        public long MinutesPlayed { get; set; }
+        public long Top1 { get; set; }
+        public long Top5 { get; set; }
+        public long Top6 { get; set; }
+        public long Top10 { get; set; }
+        public long Top12 { get; set; }
+        public long Top25 { get; set; }
+        public long Matches { get; set; }
+        public long Top3 { get; set; }
+        public DateTimeOffset DateCollected { get; set; }
+        public long Score { get; set; }
+        public long Platform { get; set; }
+        public double? TrnRating { get; set; }
+        public double? TrnRatingChange { get; set; }
+        public long PlaylistId { get; set; }
+        public long PlayersOutlived { get; set; }
+    }
+
+    public partial class Stat
+    {
+        public string Label { get; set; }
+        public string Field { get; set; }
+        public string Category { get; set; }
+        public double? ValueDec { get; set; }
+        public string Value { get; set; }
+        public string DisplayValue { get; set; }
+        public long? ValueInt { get; set; }
+        public long? Rank { get; set; }
+        public double? Percentile { get; set; }
+    }
+
+    public enum Category { General, Rating, Tops };
+}
